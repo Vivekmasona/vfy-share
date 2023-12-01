15451b698188a3b7b747789baa6c878f5caeabd9
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-01 15:05:20 +0530

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,27 +3,7 @@ import React, { FC } from "react";
 
 const Header: FC = () => {
   return (
-    <AppBar position="static" color="transparent">
-      <Container>
-        <Toolbar>
-          <Link
-            href="/"
-            sx={{
-              mr: "auto",
-            }}
-          >
-            
-          </Link>
-          <Link
-            href=""
-            target="__blank"
-            rel="noopener"
-          >
-            
-          </Link>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    
   );
 };
 
